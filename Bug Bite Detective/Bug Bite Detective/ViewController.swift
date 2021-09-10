53c690d21c8395de2a40065b305ea526990e1392
yukunl <47676210+yukunl@users.noreply.github.com>
2021-09-09 23:56:21 -0400

    Add files via upload

---
+++ Bug Bite Detective/Bug Bite Detective/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bug Bite Detective
+//
+//  Created by A-KUN on 7/31/21.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
